2037877ce67895e8104784a41c353886acc203b6
Shaik Mohammed Zakir <smzakir123@gmail.com>
2026-06-02 13:13:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll(".circle");
+const lineFill = document.getElementById("line-fill");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("previous");
+
+let currentActive = 1;
+
+nextBtn.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const progress = ((currentActive - 1) / (circles.length - 1)) * 100;
+  lineFill.style.width = progress + "%";
+}
